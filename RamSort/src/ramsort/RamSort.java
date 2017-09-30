e25a4b9703b0e2b2eced3585293df130d7742703
xeniazant <xeniazant@gmail.com>
2017-09-30 11:18:52 -0400

    moving to pc

--- RamSort/src/ramsort/RamSort.java
+++ RamSort/src/ramsort/RamSort.java
@@ -9,13 +9,65 @@ package ramsort;
  *
  * @author xeniazantello
  */
+
+import java.util.Arrays;
+import java.util.Random;
+
+
+
+
+
 public class RamSort {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+//  12 points: In the main method, invoke all the methods you've implemented (described below) at least five times 
+//  each on different inputs, for testing purposes.  Write all the inputs and the results into the console.
+//  This is the "testing" portion of your code.
     }
     
+//  24 points: Define a method named countingSort. Implement Counting Sort as in the slides/textbook. One parameter will
+// be the input array to sort. Another parameter will be a memory allocated array of the same length, for storing the 
+// output. The third parameter will be the largest single input value. You may write your code one-indexed or
+// zero-indexed, but be aware the pseudocode one-indexes the input and output while zero-indexing the auxiliary array C.
+    
+    
+    
+    
+    
+//  24 points: Define a method named randomizedQuickselect. Implement Randomized Quickselect as in the slides/textbook. 
+//  One parameter is the input array.  Two more parameters are the inclusive bounds of the subarray to consider. 
+//  One more parameter is i, the order statistic number to find the value of in the input array. This method must
+//  be nondestructive, meaning it does not reorder or modify the original input array! So, you will want to make a local 
+//  copy of the input array using java.util.Arrays.copyOf, then use the local copy for everything. You will also need 
+//  to reuse the same partition method from homework 4, which you used for quicksort.  If your partition method is not 
+//  working properly, I strongly recommend you debug it and get it working correctly before moving on to the rest of 
+//  this assignment.
+    
+    
+//    
+//    10 points extra credit: Define a method named radixSort, implementing a base-10 Radix Sort as in the 
+//    slides/textbook. This will only be a few lines of code to define the radixSort method, but you 
+//    will also need to create some private helper methods:
+
+
+//    A private helper method named getDigit to obtain and return the value of a single given base-10 digit's value from
+//    a given int value. For example, getDigit(1, 493) would return the value of the "1's place" in 493, which is 
+//    the number 3. You can make that helper method a purely math function, or instead make it with String methods.
+
+    
+    
+    
+//    A private helper method named countingSortOnDigit, which is a modified version of countingSort with an additional
+//    parameter indicating which base-10 digit is to be used for comparisons. (1 for rightmost digit, 2 for second 
+//    digit from the right, etc.) Make sure your countingSort works properly before you make this new version.  
+//    The only difference will be the line(s) of code that directly compare two elements, which will invoke digitOf 
+//    and use that single digit value, instead of comparing one entire input value with another.
+
+
+
+
+
 }
